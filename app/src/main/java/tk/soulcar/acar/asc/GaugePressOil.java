e2f5f2611efe33d0dc46d8fa0a2bab809cba9525
lookedsoup <lookrdsoup@yandex.ru>
2017-03-09 23:46:43 +0700

    добавлен GaugePressOil.java

---
+++ app/src/main/java/tk/soulcar/acar/asc/GaugePressOil.java
@@ -0,0 +1,17 @@
+package tk.soulcar.acar.asc;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GaugePressOil extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView=inflater.inflate(R.layout.gauge_press_oil, container, false);
+        return rootView;
+    }
+
+}
