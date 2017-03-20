7a4e2688ceaf93dd96ce25c7500691ecb259e7cb
lookedsoup <lookedsoup@yandex.ru>
2017-03-20 18:47:12 +0700

    мелкие изменения интерфейса

--- app/src/main/java/tk/soulcar/acar/asc/iSoulCar.java
+++ app/src/main/java/tk/soulcar/acar/asc/iSoulCar.java
@@ -1,13 +1,26 @@
 package tk.soulcar.acar.asc;
 
 import android.app.Application;
+import android.widget.Toast;
+
+import com.physicaloid.lib.Physicaloid;
 
 public class iSoulCar extends Application {
 
+    Physicaloid mPhysicaloid;
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mPhysicaloid = new Physicaloid(this);
+        mPhysicaloid.setBaudrate(9600);
+
+        if(mPhysicaloid.open()) {
+
+        } else {
+            Toast.makeText(this, "Cannot open", Toast.LENGTH_LONG).show();
+        }
 
         UsbOtg.initInstance();
     }
